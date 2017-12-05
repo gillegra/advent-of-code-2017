56f57fe3c7eb11902e8afaeee0bfd49c57083d16
Grant Gillespie <grant@sntmedia.com>
2017-12-04 19:12:10 -0600

    solved day 1 part 2

--- golang/day01.go
+++ golang/day01.go
@@ -17,25 +17,38 @@ func main() {
 	check(err)
 	fmt.Println(len(input))
 
-	inputDecoded := make([]int, 0, int(len(input)/2)+1) //give it some reasonable capacity
+	inputDecoded := make([]int, 0, int(len(input))) //give it some reasonable capacity
 	err = json.Unmarshal(input, &inputDecoded)
 	check(err)
 	// fmt.Println(len(inputDecoded))
 	// fmt.Println(cap(inputDecoded))
 	// fmt.Println(inputDecoded)
 
+	length := len(inputDecoded)
 	inputDecoded = append(inputDecoded, inputDecoded[0])
 	// fmt.Println(len(inputDecoded))
 	// fmt.Println(cap(inputDecoded))
 
 	var sum int64
-	length := len(inputDecoded)
 	for i, num := range inputDecoded {
-		if i < length-1 && num == inputDecoded[i+1] {
+		if i < length && num == inputDecoded[i+1] {
 			// fmt.Printf("%d += %d\n", sum, num)
 			sum += int64(num)
 		}
 	}
 
-	fmt.Printf("Results: %d\n", sum)
+	fmt.Printf("Results 1: %d\n", sum)
+
+	inputDecoded = append(inputDecoded[:len(inputDecoded)-1], inputDecoded[:len(inputDecoded)/2]...)
+	step := length / 2
+	// newLength := len(inputDecoded)
+	sum = 0 //re-zero this for reuse
+
+	for i, num := range inputDecoded {
+		if i < length && num == inputDecoded[i+step] {
+			sum += int64(num)
+		}
+	}
+
+	fmt.Printf("Results %d: %d\n", step, sum)
 }
