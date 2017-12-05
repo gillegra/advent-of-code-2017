6f5c03241a22b86e799dd5049f6512b5e6c66667
Grant Gillespie <grant@sntmedia.com>
2017-12-04 18:39:54 -0600

    solved day 1 part 1

--- golang/day01.go
+++ golang/day01.go
@@ -17,9 +17,25 @@ func main() {
 	check(err)
 	fmt.Println(len(input))
 
-	inputDecoded := make([]int, 0, int(len(input)/2)) //give it some reasonable capacity
+	inputDecoded := make([]int, 0, int(len(input)/2)+1) //give it some reasonable capacity
 	err = json.Unmarshal(input, &inputDecoded)
 	check(err)
-	fmt.Println(len(inputDecoded))
+	// fmt.Println(len(inputDecoded))
+	// fmt.Println(cap(inputDecoded))
 	// fmt.Println(inputDecoded)
+
+	inputDecoded = append(inputDecoded, inputDecoded[0])
+	// fmt.Println(len(inputDecoded))
+	// fmt.Println(cap(inputDecoded))
+
+	var sum int64
+	length := len(inputDecoded)
+	for i, num := range inputDecoded {
+		if i < length-1 && num == inputDecoded[i+1] {
+			// fmt.Printf("%d += %d\n", sum, num)
+			sum += int64(num)
+		}
+	}
+
+	fmt.Printf("Results: %d\n", sum)
 }
